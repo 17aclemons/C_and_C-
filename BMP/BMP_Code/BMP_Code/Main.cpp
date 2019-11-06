705fb6549e77c246fd4a7f082d6415d9410565f1
17aclemons <17aclemons@gmail.com>
2019-11-06 11:39:55 -0700

    stuff

---
+++ BMP/BMP_Code/BMP_Code/Main.cpp
@@ -0,0 +1,20 @@
+/*Andrew Clemons
+C and C++
+BMP Assignment
+Fall 2019
+*/
+
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+typedef char BYTE;
+
+int main() {
+
+	ifstream inf("1_Created.bmp");
+	BYTE b = 0xff; 
+
+
+}
